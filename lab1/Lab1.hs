3546dfff2c4b7b63c282d3432c784552be7ac90b
MeepShake <susselelle@hotmail.com>
2024-11-04 22:10:42 +0100

    lab1 progress

--- lab1/Lab1.hs
+++ lab1/Lab1.hs
@@ -6,7 +6,7 @@
 --------------------------------------------
 power :: Integer -> Integer -> Integer
 power n k
-   | k < 0 = error "power: negative argument"
+   | k < 0 = error "Negative exponent not supported"
 power n 0  = 1
 power n k  = n * power n (k-1)
 
@@ -15,28 +15,39 @@ power n k  = n * power n (k-1)
 -- power n k takes to compute
 
 stepsPower :: Integer -> Integer -> Integer
-stepsPower n k = undefined 
+stepsPower n 0         = 1
+stepsPower n k | k > 0 = k + 1
+               | otherwise = error "Negative exponent not supported"
 
 
 -- B -------------------------
 -- power1
-
-power1 = undefined
+power1 :: Integer -> Integer -> Integer
+power1 n 0             = 1
+power1 n k | k > 0     = product [ n | l <- [1..k] ]
+           | otherwise = error "Negative exponent not supported"
 
 -- C -------------------------
 -- power2
 
-power2 = undefined
+power2 :: Integer -> Integer -> Integer
+power2 n 0             = 1
+power2 n k | even k    = (n * n) * power2 n (k `div` 2)
+           | odd k     = n * power2 n (k-1)
+           | otherwise = error "Negative exponent not supported"
 
 -- D -------------------------
 {- 
 
 <Describe your test cases here>
-
+   case 1: test with a negative number
+   case 2: test with a zero
+   case 3: test with a positive number greater than 0
  -}
 
 -- 
-prop_powers = undefined
+prop_powers n k = power n k' == power1 n k'
+   where k' = abs k
 
 --
 powerTest :: Bool
