ece1498b73fa1e390c605aaab8eeb97bdbf55a0b
MeepShake <susselelle@hotmail.com>
2024-11-13 16:25:57 +0100

    fixed

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -57,7 +57,8 @@ hand2 = Add (Card (Numeric 2) Hearts)
 
 sizeSteps :: [Integer]
 sizeSteps = [ size hand2
-            , size (Add (Card (Numeric 2) Hearts)
-                        (Add (Card Jack Spades) Empty))
-            -- ... add the remaining steps here
+            , size (Add (Card (Numeric 2) Hearts)(Add (Card Jack Spades) Empty))
+            , 1 + (size (Add (Card Jack Spades) Empty))
+            , 1 + (1 + size Empty)
+            , 1 + 1 + 0
             , 2]
