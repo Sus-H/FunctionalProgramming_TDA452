8bc411bdf98c141418f58b6bb7ff5a634759b658
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-12-09 16:00:24 +0100

    C done
    we hope

---
+++ Lab4/Expr.hs
@@ -0,0 +1,55 @@
+import Prelude hiding (sin,cos)
+import qualified Prelude as P
+
+data Expr = Add Expr Expr 
+          | Mul Expr Expr 
+          | Sin Expr 
+          | Cos Expr 
+          | Num Double 
+          | X 
+
+x :: Expr
+x = X
+
+num :: Double -> Expr
+num a = Num a
+
+add,mul :: Expr -> Expr -> Expr
+add a b = Add a b
+mul a b = Mul a b
+
+sin,cos :: Expr -> Expr
+sin a = Sin a
+cos a = Cos a 
+
+-- Calculates the number of operations
+size :: Expr -> Int
+size (Num a) = 0
+size (Sin a) = 1 + size a
+size (Cos a) = 1 + size a
+size (Add a b) = 1 + size a + size b
+size (Mul a b) = 1 + size a + size b
+
+-- Prints the expression
+showExpr :: Expr -> String
+showExpr (Num a)   = show a
+showExpr X         = "x"
+showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
+showExpr (Mul a b) = showFactor a ++ " * " ++ showFactor b
+   where showFactor (Add e e') = "(" ++ showExpr (Add e e')  ++ ")"
+         showFactor e          = showExpr e
+showExpr (Sin a) = "Sin(" ++ showExpr a ++ ")"
+showExpr (Cos a) = "Cos(" ++ showExpr a ++ ")"
+
+-- Evaluates an expression given the expression and the variable X
+eval :: Expr -> Double -> Double
+eval (Num a) _ = a
+eval X b = b
+eval (Add a b) c = eval a c + eval b c
+eval (Mul a b) c = eval a c * eval b c
+eval (Sin a) c = P.sin (eval a c)
+eval (Cos a) c = P.cos (eval a c)
+
+-- this does something I don't yet know what it is
+readExpr :: String -> Maybe Expr
+readExpr = undefined
