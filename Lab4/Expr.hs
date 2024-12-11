4221aadf217b732d581771b15824c3c7d5941ab3
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-12-11 18:53:06 +0100

    refactoring
    redid the data type for expr and fixed all the code to use that version instead

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,14 +1,19 @@
 import Prelude hiding (sin,cos)
 import qualified Prelude as P
+import Test.QuickCheck
+import Data.Maybe
 
 import Parsing
 
-data Expr = Add Expr Expr 
-          | Mul Expr Expr 
-          | Sin Expr 
-          | Cos Expr 
+data Expr = BinOp Op Expr Expr 
+          | UnOp TrigOp Expr
           | Num Double 
           | X 
+    deriving (Eq, Show)
+
+data Op = Add | Mul
+    deriving (Eq, Show)
+data TrigOp = Sin | Cos 
     deriving (Eq, Show)
 
 x :: Expr
@@ -18,74 +23,64 @@ num :: Double -> Expr
 num a = Num a
 
 add,mul :: Expr -> Expr -> Expr
-add a b = Add a b
-mul a b = Mul a b
+add a b = BinOp Add a b
+mul a b = BinOp Mul a b
 
 sin,cos :: Expr -> Expr
-sin a = Sin a
-cos a = Cos a 
-
-doubleParser = do
-    -- natural <- sat elem ['1'..'9']
-    -- nonNegative <- char '0' <|> natural
-    nonNegative <- sat elem ['0'..'9']
-    decimal <- char '.' 
-    double <- digit <*> decimal <*> nonNegative
-    return $ read double
-
-doubleParser = do
-    sign <- optional (char '-')
-    integral <- some digitChar <|> string "0"
-    fractional <- optional $ do
-        decimal <- char '.'
-        digits <- some digitChar
-        return (decimal : digits)
+sin a = UnOp Sin a
+cos a = UnOp Cos a 
 
 -- Calculates the number of operations
 size :: Expr -> Int
 size (Num a) = 0
-size (Sin a) = 1 + size a
-size (Cos a) = 1 + size a
-size (Add a b) = 1 + size a + size b
-size (Mul a b) = 1 + size a + size b
+size (UnOp _ b) = 1 + size b
+size (BinOp _ b c) = 1 + size b + size c
 
--- Prints the expression
+-- Prints the expression (turns an expression to a String)
 showExpr :: Expr -> String
 showExpr (Num a)   = show a
 showExpr X         = "x"
-showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
-showExpr (Mul a b) = showFactor a ++ " * " ++ showFactor b
-   where showFactor (Add e e') = "(" ++ showExpr (Add e e')  ++ ")"
-         showFactor e          = showExpr e
-showExpr (Sin a) = "Sin(" ++ showExpr a ++ ")"
-showExpr (Cos a) = "Cos(" ++ showExpr a ++ ")"
+-- showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
+-- showExpr (Mul a b) = showFactor a ++ " * " ++ showFactor b
+showExpr (BinOp c a b) = case c of 
+    Mul -> showFactor a ++ " * " ++ showFactor b
+    _   -> showExpr a ++ " + " ++ showExpr b
+   where
+        showFactor (BinOp Add e e') = "(" ++ showExpr (BinOp Add e e')  ++ ")"
+        showFactor e          = showExpr e
+showExpr (UnOp trigOperator expression) = case expression of
+    Num a -> show trigOperator ++ show a
+    X     -> show trigOperator ++ "x"
+    _     -> show trigOperator ++ "(" ++ showExpr expression ++ ")"
 
 -- Evaluates an expression given the expression and the variable X
 eval :: Expr -> Double -> Double
 eval (Num a) _ = a
 eval X b = b
-eval (Add a b) c = eval a c + eval b c
-eval (Mul a b) c = eval a c * eval b c
-eval (Sin a) c = P.sin (eval a c)
-eval (Cos a) c = P.cos (eval a c)
+eval (BinOp a b c) d = case a of
+    Add -> eval b d + eval c d
+    Mul -> eval b d * eval c d
+eval (UnOp a b) c = case a of
+    Sin -> P.sin (eval b c)
+    Cos -> P.cos (eval b c)
 
--- this does something I don't yet know what it is
+-- -- Turns a String into an expression
 readExpr :: String -> Maybe Expr
-readExpr s = case parse expr s of
+readExpr s = case parse expr $ filter (/=' ') s of
     Just (a,"") -> Just a
     _           -> Nothing
     where
-        expr = foldl1 Add <$> chain term (char '+')
-        term = foldl1 Mul <$> chain factor (char '*')
+        expr = foldl1 (BinOp Add) <$> chain term (char '+') 
+        term = foldl1 (BinOp Mul) <$> chain factor (char '*')
         factor = Num <$> number <|> char '(' *> expr <* char ')' 
              <|> siinn <|> cooss <|> xxx 
           where
             cooss = do
                 w <- (trig "cos" factor)
-                return $ Cos w
+                return $ UnOp Cos w
             siinn = do
                 w <- trig "sin" factor
-                return $ Sin w
+                return $ UnOp Sin w
             xxx = do
                 char 'x'
                 return x
@@ -94,11 +89,45 @@ readExpr s = case parse expr s of
 trig :: String -> Parser Expr -> Parser Expr
 trig s t = foldl1 (*>) [char c | c <- s] *> t
 
+-- Parser for a double
 number :: Parser Double
-number =  read <$> oneOrMore digit
+number = read <$> do
+    ds   <- oneOrMore digit
+    rest <- decimals <|> return ""
+    return (ds ++ rest)
+    where
+        decimals = do 
+            c      <- char '.'
+            smalls <- oneOrMore digit
+            return (c:smalls)
 
+-- Test that given an expression, which is put through our two functions
+-- showExpr and readExpr will produce the original input
 prop_ShowReadExpr :: Expr -> Bool
-prop_ShowReadExpr = undefined
+prop_ShowReadExpr ex = case c of 
+    Just _  -> True
+    Nothing -> False
+    where
+        c = readExpr $ showExpr ex
 
+-- Generate an expression of limited size
 arbExpr :: Int -> Gen Expr
-arbExpr = undefined
+arbExpr n = frequency
+    [(20, Num <$> elements[0..10]), 
+     (15, do
+        a <- arbExpr (n `div` 2)
+        b <- arbExpr (n `div` 2)
+        op <- elements [Add, Mul]
+        return $ BinOp op a b),
+     (1, do
+        a <- arbExpr (n - 1)
+        trigOp <- elements [Sin, Cos]
+        return $ UnOp trigOp a),
+     (10, do return X)]
+
+instance Arbitrary Expr where 
+  arbitrary = sized arbExpr
+
+simplify :: Expr -> Expr
+simplify e = undefined
+
