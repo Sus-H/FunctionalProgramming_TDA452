a2483a05ad341cb7f6d9874686cda81eeaccf1e4
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-12-11 20:49:29 +0100

    F done

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -132,6 +132,66 @@ arbExpr n = frequency
 instance Arbitrary Expr where 
   arbitrary = sized arbExpr
 
-simplify :: Expr -> Expr
-simplify e = undefined
+-- Examples
+-- (BinOp Mul (UnOp Sin (Num 0)) (BinOp Add (Num 3) (Num 3)))
 
+-- Simplifying expressions
+simplify :: Expr -> Expr
+simplify (Num a) = Num a
+simplify X       = x
+
+simplify (BinOp oper (Num a) (Num b)) = case oper of
+    Add -> Num (a + b)
+    Mul -> Num (a * b)
+simplify (BinOp oper e (Num 0.0))     = case oper of 
+    Add -> simplify e
+    Mul -> Num 0
+simplify (BinOp oper (Num 0.0) e)     = case oper of
+    Add -> simplify e
+    Mul -> Num 0
+
+simplify (BinOp oper (Num a) X) = case oper of
+    Add -> (BinOp Add (Num a) X)
+    Mul -> (BinOp Mul (Num a) X)
+simplify (BinOp oper X (Num a)) = case oper of
+    Add -> (BinOp Add X (Num a))
+    Mul -> (BinOp Mul X (Num a))
+
+simplify (BinOp Mul e (Num 1)) = simplify e
+simplify (BinOp Mul (Num 1) e) = simplify e
+simplify (BinOp Mul X e) = (BinOp Mul x (simplify e))
+simplify (BinOp Mul e X) = (BinOp Mul (simplify e) x)
+
+simplify (BinOp oper e1 e2) = simplify $ BinOp oper (simplify e1) (simplify e2)
+
+simplify (UnOp trig (Num a)) = case trig of
+    Sin -> Num $ P.sin a
+    Cos -> Num $ P.cos a
+simplify (UnOp trig X) = (UnOp trig X)
+simplify (UnOp t expr) = simplify $ UnOp t (simplify expr)
+
+-- G
+--Differentiate function an simplify
+-- expression
+-- simplifyAndDifferentiate :: Expr -> Expr
+-- simplifyAndDifferentiate expr = simplify $ differentiate expr
+
+-- -- Differentiates an expression
+-- differentiate :: Expr -> Expr
+-- differentiate (Num _) = Num 0
+-- differentiate Var = Num 1
+
+-- differentiate (BinOp Mul Var Var) = BinOp Mul (Num 2) Var
+
+-- differentiate (BinOp Add e1 e2) =
+--     simplify $ BinOp Add (differentiate e1) (differentiate e2)
+
+-- differentiate (BinOp Mul e1 e2) =
+--     simplify $ BinOp Add (BinOp Mul (differentiate e1) e2)
+--         (BinOp Mul e1 (differentiate e2))
+-- differentiate (UnOp Sin expr) = 
+--     simplify $ BinOp Mul (differentiate expr) (UnOp Cos expr)
+
+-- differentiate (UnOp Cos e1) = 
+--     simplify $ BinOp Mul (Num (-1)) 
+--         (simplify $ BinOp Mul (differentiate e1) (UnOp Sin e1))
