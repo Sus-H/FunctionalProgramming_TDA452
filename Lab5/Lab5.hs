87e34d2aba88f24f00f109e5b80c4fd9f614cf83
Paulina Palmberg <PaulinaPalmberg@users.noreply.github.com>
2024-12-12 16:07:47 +0100

    starting with data types

--- Lab5/Lab5.hs
+++ Lab5/Lab5.hs
@@ -1,1 +1,16 @@
--- This is the file for lab 5 
+-- This is the file for lab 5, a simlpe guessing game
+
+data QA = Question String Sub Sub
+    deriving Show
+data Sub = SubQA QA | Person String 
+    deriving Show
+
+exampleQA = Question "Is it an actor?" 
+    (Person "Person 1") (SubQA (Question "Is it a writer?" 
+                        (Person "Person 2") (Person "Person 3")))
+
+play :: QA -> IO QA
+play = undefined
+
+main :: IO ()
+main = undefined
